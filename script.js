765b263c67990d8c8952057713a81ca457bc520d
Penélope Vieira <vieirapenelope21@gmail.com>
2026-02-08 20:22:38 -0300

    Implement task management functions in script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let tarefas = []
 function adicionarTarefa() {
     //toda o codigo é executado em ordem quando o botão é clicado.
 
@@ -21,19 +22,75 @@ function adicionarTarefa() {
         let mensagemSucesso = "task adicionada com sucesso!";
         mensagem.style.color = "#ff8282"
         mensagem.textContent = mensagemSucesso
-    //pega o elemento html com id mensagemSucesso e muda o texto para o valor da variavel mensagem, ou seja, "task adicionada com sucesso!".
+        //pega o elemento html com id mensagemSucesso e muda o texto para o valor da variavel mensagem, ou seja, "task adicionada com sucesso!".
     
-        const listatarefas = document.getElementById("listatarefas")
-        //pega o elemento html com id listatarefas e guarda na variavel listatarefas, para depois usar a variavel listatarefas para adicionar a tarefa na lista de tarefas.
-        let novatarefa = document.createElement("li")
-        //a variavel novatarefa cria um li, a lista.
-        novatarefa.textContent = addtarefa
-        //o texto do li criado na variavel novatarefa recebe o valor da variavel addtarefa, ou seja, o valor digitado no input.
-        listatarefas.appendChild(novatarefa)
-        //a variavel listatarefas, que é a ul, recebe um filho, que é a variavel novatarefa, ou seja, o li criado.
+        tarefas.push(addtarefa)
+        //adiciona o valor da variavel addtarefa, ou seja, o valor digitado no input, na lista de tarefas, ou seja, na variavel tarefas.
+        renderizarTarefas()
+        //chama a função renderizarTarefas para mostrar a lista de tarefas atualizada.
+
     }
 
     tarefa.value = "";
     //limpa o input ja que ele fica vazio no final da função.
 
+}
+
+function renderizarTarefas() {
+        const listatarefas = document.getElementById("listatarefas")
+        listatarefas.innerHTML = ""
+         //pega o elemento html com id listatarefas e guarda na variavel listatarefas, para depois usar a variavel listatarefas para adicionar a tarefa na lista de tarefas.
+        
+        // for itens na lista
+        //1. item inicial (iterador)
+        //2. item final (condição de parada)
+        //3. se vai de um em um elemento ou de outro valor (incremento/frequencia)
+        
+        //for (iterador, condição de parada, incremento)
+
+        //i++ é o mesmo que i = i + 1, ou seja, o iterador i recebe o valor de i mais 1, ou seja, ele vai de um em um elemento da lista.
+
+        for (let i = 0; i < tarefas.length; i++){
+        let novatarefa = document.createElement("li")
+        //a variavel novatarefa cria um li, a lista.
+        novatarefa.textContent = tarefas[i]
+        //o texto do li criado na variavel novatarefa recebe o valor da variavel tarefas[i], ou seja, o valor da tarefa na posição i da lista de tarefas.
+        
+        let botaoRemover = document.createElement("button")
+        botaoRemover.className= "remover"
+        botaoRemover.textContent = "remover"
+        botaoRemover.onclick = () => removerTarefa(i)
+
+        let botaoEditar = document.createElement("button")
+        botaoEditar.className= "editar"
+        botaoEditar.textContent = "editar"
+        botaoEditar.onclick = () => editarTarefa(i)
+
+        novatarefa.appendChild(botaoRemover)
+        //a variavel novatarefa, que é o li criado, recebe um filho, que é a variavel botaoRemover, ou seja, o button criado.
+        novatarefa.appendChild(botaoEditar)
+        //a variavel novatarefa, que é o li criado, recebe um filho, que é a variavel botaoEditar, ou seja, o button criado.
+        listatarefas.appendChild(novatarefa)
+        //a variavel listatarefas, que é a ul, recebe um filho, que é a variavel novatarefa, ou seja, o li criado.
+        }
+  
+}
+
+function removerTarefa(i) {
+    tarefas.splice(i, 1)
+    renderizarTarefas()
+}
+
+function editarTarefa(i) {
+    let  tarefaeditada = prompt("edite a tarefa:")
+    if (tarefaeditada.trim() !== "") {
+        tarefas[i] = tarefaeditada
+        renderizarTarefas()
+    }
+}
+
+function limparLista() {
+    tarefas.length = 0
+    renderizarTarefas()
+    mensagem.textContent = "Lista limpa com sucesso!"
 }
